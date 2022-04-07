b048d048cc55e9d8bcbc6c973950b479535dc4d4
wenjian80 <wenjian80@gmail.com>
2022-04-07 10:11:15 +0800

    Update MainTest.java

--- src/test/java/io/helidon/examples/quickstart/se/MainTest.java
+++ src/test/java/io/helidon/examples/quickstart/se/MainTest.java
@@ -18,6 +18,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MainTest {
 
